8db65e9b178a9c108142466dc888713614e5ff08
Bukola <bukola_jimoh@yahoo.co.uk>
2019-03-29 10:18:08 +0000

    ading employee

--- src/Components/EmployeeInfo/index.js
+++ src/Components/EmployeeInfo/index.js
@@ -4,17 +4,77 @@ import css from "./EmployeeInfo.module.css";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+let searchValue = "";
 class EmployeeInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      employeeNumber: "",
+      position: ""
+    };
+  }
+
+  handleFirstName = event => {
+    searchValue = event.target.value;
+    this.setState(() => ({ firstName: searchValue }));
+    console.log(this.state.firstName);
+  };
+
+  handleLastName = event => {
+    searchValue = event.target.value;
+    this.setState(() => ({ lastName: searchValue }));
+  };
+
+  handleEmployeeNumber = event => {
+    searchValue = event.target.value;
+    this.setState(() => ({ employeeNumber: searchValue }));
+  };
+
+  handleSubmit = () => {
+    fetch(`http://localhost:5000/employees`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json" // <-- Specifying the Content-Type
+      },
+      body: JSON.stringify({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        staffNumber: this.state.employeeNumber,
+        position: ""
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        alert(response.message);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   render() {
     return (
       <div className={css.masterContainer}>
-        <h1>Add Employee</h1>
+        <h1>Add Employee </h1>
         <div className={css.container}>
-          <TextField placeholder="First Name" />
-          <TextField placeholder="Surname" />
-          <TextField placeholder="Employee Number" />
+          <TextField
+            placeholder="First Name"
+            onChange={event => this.handleFirstName(event)}
+          />
+          <TextField
+            placeholder="Surname"
+            onChange={event => this.handleLastName(event)}
+          />
+          <TextField
+            placeholder="Employee Number"
+            onChange={event => this.handleEmployeeNumber(event)}
+          />
           <br />
-          <Button color="secondary">Submit</Button>
+          <Button color="secondary" onClick={this.handleSubmit}>
+            Submit
+          </Button>
         </div>
       </div>
     );
